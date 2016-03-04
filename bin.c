71db915ba75c50b0cf00aa54f20777dc69ea3102
Dharanimozhi <dharanimozhi@gmail.com>
2016-03-04 17:30:48 +0530

    Create bin.c

---
+++ bin.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+int a[20],n,i,num;
+printf("enter the number of elements in the array");
+scanf("%d",&n);
+printf("enter the elements of the array");
+for(i=1;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("enter the element to be searched");
+scanf("%d",&num);
+int j=n/2;
+if(num==a[j])
+{
+printf("element is at the index %d",j);
+}
+else if(num<a[j])
+{
+for(i=1;i<j;i++)
+{
+if(num==a[i])
+printf("element is at the index %d",i);
+}
+}
+else
+{
+for(i=(j+1);i<=n;i++)
+{
+if(num==a[i])
+printf("element is at the index %d",i);
+}
+}
+return 0;
+}
